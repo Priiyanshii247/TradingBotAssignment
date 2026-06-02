d11b2e4d3f353ab22462bc2ebc479c7c68f4fde1
Priyanshi Shrivastava <priyanshii2407@gmail.com>
2026-06-02 20:39:11 +0530

    Remove test and debug files

--- test_ping.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://demo-fapi.binance.com/fapi/v1/ping"
-
-response = requests.get(url)
-
-print("Status Code:", response.status_code)
-print("Response:", response.text)
